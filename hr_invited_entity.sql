13e36fdca911559f74d2ad75d7bafa09333a84ad
Prakriti <nature.prakriti123@gmail.com>
2023-02-28 22:02:25 +0530

    prakriti: updated hr_invited

--- hr_invited_entity.sql
+++ hr_invited_entity.sql
@@ -1,27 +1,23 @@
 create database hr_invited;
 use hr_invited;
-DROP TABLE IF EXISTS hr_invited.hr;
+DROP TABLE IF EXISTS placement_management_system.hr;
 
-CREATE TABLE hr_invited.hr ( 
+CREATE TABLE placement_management_system.hr ( 
   email_id VARCHAR(255) UNIQUE NOT NULL,
   CHECK (email_id LIKE '%@%.%' AND email_id NOT LIKE '%@%@%'),
   hr_status VARCHAR(50) NOT NULL,
-  company_name VARCHAR(100) NOT NULL
+  company_name VARCHAR(100) NOT NULL,
+  parent_id int,
+  constraint mailed_to FOREIGN KEY(parent_id) REFERENCES administrator(person_id)
+   on delete cascade
+   on update cascade
 );
 
-INSERT INTO hr_invited.hr (email_id, hr_status, company_name)
-SELECT p.email_id, 
-       CASE 
-         WHEN RAND() < 0.25 THEN 'called' 
-         WHEN RAND() BETWEEN 0.25 AND 0.5 THEN 'Mail sent' 
-         WHEN RAND() BETWEEN 0.5 AND 0.75 THEN 'Reply received' 
-         ELSE 'Received details' 
-       END AS hr_status, 
-       cd.company_name
-FROM people.person p
-JOIN people.company_details cd ON p.person_id = cd.company_id
-WHERE p.email_id NOT IN (SELECT email_id FROM hr_invited.hr);
+INSERT INTO placement_management_system.hr (email_id, hr_status, company_name, parent_id)
+values
+('prakriti.saroj@iitgn.ac.in', 'mailed', 'google', 11007890),
+('prakriti.saroj1@iitgn.ac.in', 'mailed', 'google', 11007890);
 
 
-SELECT * FROM hr_invited.hr;
+SELECT * FROM placement_management_system.hr where parent_id = 11007890;
 
